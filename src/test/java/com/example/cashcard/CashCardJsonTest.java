00f551a816e903dcae776e1d40269e52960c9121
kevinantunezortiz <kevinantunetiz@gmail.com>
2025-03-24 21:47:27 -0600

    Agregar pruebas unitarias para CashCardJson

---
+++ src/test/java/com/example/cashcard/CashCardJsonTest.java
@@ -0,0 +1,11 @@
+package com.example.cashcard;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+public class CashCardJsonTest {
+    @Test
+    public void myFirstTest() {
+        assertThat(42).isEqualTo(42);
+    }
+    
+}
