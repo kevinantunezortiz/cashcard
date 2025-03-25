6172615a8075ce1878b3a77b46bdb44e6d8c1a78
kevinantunezortiz <kevinantunetiz@gmail.com>
2025-03-24 22:43:21 -0600

    agregaron pruebas de deserializacion

--- src/test/java/com/example/cashcard/CashCardJsonTest.java
+++ src/test/java/com/example/cashcard/CashCardJsonTest.java
@@ -25,11 +25,35 @@ public class CashCardJsonTest {
     @Test
     void testSerialize() throws IOException {
         CashCard cashCard = new CashCard(99L, 123.45);
+        //read the expected JSON from a file
         String expectedJson = new String(Files.readAllBytes(Paths.get("src/test/resources/expected.json")));
+
+        //verify that the JSON matches the expected JSON 
         assertThat(json.write(cashCard)).isEqualToJson(expectedJson);
+        //verify that the JSON has a specific value
         assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.id");
+        //VERIFICA QUE EL VALOR DEl campo sea 99
         assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.id").isEqualTo(99);
+        //VERIFICA QUE EL VALOR DEl campo sea 123.45 Y QUE EXISTA LA PROPIEDAD AMOUNT
         assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.amount");
         assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.amount").isEqualTo(123.45);
+    }
+
+    @Test
+    void cashCardDeserializationTest() throws IOException {
+        //read the expected JSON from a file
+        String expected = """
+           {
+               "id":99,
+               "amount":123.45
+           }
+           """;
+        //verify that the JSON matches the expected JSON
+        assertThat(json.parse(expected))
+                .isEqualTo(new CashCard(99L, 123.45));
+        //verify that the JSON has a id field with a specific value equals 1000
+        assertThat(json.parseObject(expected).id()).isEqualTo(99L);
+        //verify that the JSON has a amount field with a specific value equals 67.89
+        assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
     }
 }
